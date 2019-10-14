c2f062a62f1b643bcb9b95c5f57a4d79e177f111
Thomas Moors <thomas.moors@ihomer.nl>
2019-10-14 16:14:17 +0200

    Joep fix

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -66,18 +66,19 @@
 
 <script>
     import { FormField, HandlesValidationErrors } from 'laravel-nova'
-    import { library } from '@fortawesome/fontawesome-pro'
+    import { library } from '@fortawesome/fontawesome-svg-core'
     // import { fas } from '@fortawesome/free-solid-svg-icons'
     // import { far } from '@fortawesome/free-regular-svg-icons'
-    // import { fab } from '@fortawesome/free-brands-svg-icons'
+    import { fab } from '@fortawesome/free-brands-svg-icons'
 
     import { fas } from '@fortawesome/pro-solid-svg-icons'
     import { far } from '@fortawesome/pro-regular-svg-icons'
     import { fal } from '@fortawesome/pro-light-svg-icons'
-    import { fad } from '@fortawesome/pro-duotone-svg-icons'
+    // import { fad } from '@fortawesome/pro-duotone-svg-icons'
 
     // Add all icons to the library so you can use it in your page
-    library.add(fas, far, fal, fad)
+
+    library.add(fas, far, fal, fab)
 
     import '@fortawesome/fontawesome-free/js/fontawesome.js'
 
