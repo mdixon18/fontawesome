9a6acd586abc38936ed20f1ba327e38488b7e203
Matthew <matthew@httpware.co.uk>
2020-09-23 20:38:32 +0100

    Fix bug on index and detail

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -9,6 +9,33 @@
 <script>
 export default {
   props: ["resource", "resourceName", "resourceId", "field"],
+
+  beforeMount() {
+    const { library } = require("@fortawesome/fontawesome-svg-core");
+    const fab = require("@fortawesome/free-brands-svg-icons").fab;
+
+    if (this.pro) {
+      const fas = require("@fortawesome/pro-solid-svg-icons").fas;
+      const far = require("@fortawesome/pro-regular-svg-icons").far;
+      const fal = require("@fortawesome/pro-light-svg-icons").fal;
+      const fad = require("@fortawesome/pro-duotone-svg-icons").fad;
+      require("@fortawesome/fontawesome-pro/js/fontawesome.js");
+
+      library.add(fas, far, fab, fal, fad);
+    } else {
+      const fas = require("@fortawesome/free-solid-svg-icons").fas;
+      const far = require("@fortawesome/free-regular-svg-icons").far;
+      require("@fortawesome/fontawesome-free/js/fontawesome.js");
+
+      library.add(fas, far, fab);
+    }
+  },
+
+  computed: {
+    pro() {
+      return this.field.pro || false;
+    },
+  },
 };
 </script>
 
